5056874fd6d5fc38953521cc43fc1eafa17136b1
bggd <gameovera@live.com>
2017-08-22 22:06:50 +0900

    Change API

--- src/mat4.cpp
+++ src/mat4.cpp
@@ -1,44 +1,51 @@
-gmath::Mat4 gmath::mat4::mul(const gmath::Mat4& A, const gmath::Mat4& B)
+namespace gmath {
+
+mat4 mat4::mul(const mat4& left, const mat4& right)
 {
-  gmath::Mat4 ret;
+  const float* A = left.elements;
+  const float* B = right.elements;
 
-  ret.m00 = A.m00*B.m00 + A.m04*B.m01 + A.m08*B.m02 + A.m12*B.m03;
-  ret.m01 = A.m01*B.m00 + A.m05*B.m01 + A.m09*B.m02 + A.m13*B.m03;
-  ret.m02 = A.m02*B.m00 + A.m06*B.m01 + A.m10*B.m02 + A.m14*B.m03;
-  ret.m03 = A.m03*B.m00 + A.m07*B.m01 + A.m11*B.m02 + A.m15*B.m03;
+  mat4 ret = {
+    A[0]*B[0] + A[4]*B[1] + A[8]*B[2] + A[12]*B[3],
+    A[1]*B[0] + A[5]*B[1] + A[9]*B[2] + A[13]*B[3],
+    A[2]*B[0] + A[6]*B[1] + A[10]*B[2] + A[14]*B[3],
+    A[3]*B[0] + A[7]*B[1] + A[11]*B[2] + A[15]*B[3],
 
-  ret.m04 = A.m00*B.m04 + A.m04*B.m05 + A.m08*B.m06 + A.m12*B.m07;
-  ret.m05 = A.m01*B.m04 + A.m05*B.m05 + A.m09*B.m06 + A.m13*B.m07;
-  ret.m06 = A.m02*B.m04 + A.m06*B.m05 + A.m10*B.m06 + A.m14*B.m07;
-  ret.m07 = A.m03*B.m04 + A.m07*B.m05 + A.m11*B.m06 + A.m15*B.m07;
+    A[0]*B[4] + A[4]*B[5] + A[8]*B[6] + A[12]*B[7],
+    A[1]*B[4] + A[5]*B[5] + A[9]*B[6] + A[13]*B[7],
+    A[2]*B[4] + A[6]*B[5] + A[10]*B[6] + A[14]*B[7],
+    A[3]*B[4] + A[7]*B[5] + A[11]*B[6] + A[15]*B[7],
 
-  ret.m08 = A.m00*B.m08 + A.m04*B.m09 + A.m08*B.m10 + A.m12*B.m11;
-  ret.m09 = A.m01*B.m08 + A.m05*B.m09 + A.m09*B.m10 + A.m13*B.m11;
-  ret.m10 = A.m02*B.m08 + A.m06*B.m09 + A.m10*B.m10 + A.m14*B.m11;
-  ret.m11 = A.m03*B.m08 + A.m07*B.m09 + A.m11*B.m10 + A.m15*B.m11;
+    A[0]*B[8] + A[4]*B[9] + A[8]*B[10] + A[12]*B[11],
+    A[1]*B[8] + A[5]*B[9] + A[9]*B[10] + A[13]*B[11],
+    A[2]*B[8] + A[6]*B[9] + A[10]*B[10] + A[14]*B[11],
+    A[3]*B[8] + A[7]*B[9] + A[11]*B[10] + A[15]*B[11],
 
-  ret.m12 = A.m00*B.m12 + A.m04*B.m13 + A.m08*B.m14 + A.m12*B.m15;
-  ret.m13 = A.m01*B.m12 + A.m05*B.m13 + A.m09*B.m14 + A.m13*B.m15;
-  ret.m14 = A.m02*B.m12 + A.m06*B.m13 + A.m10*B.m14 + A.m14*B.m15;
-  ret.m15 = A.m03*B.m12 + A.m07*B.m13 + A.m11*B.m14 + A.m15*B.m15;
+    A[0]*B[12] + A[4]*B[13] + A[8]*B[14] + A[12]*B[15],
+    A[1]*B[12] + A[5]*B[13] + A[9]*B[14] + A[13]*B[15],
+    A[2]*B[12] + A[6]*B[13] + A[10]*B[14] + A[14]*B[15],
+    A[3]*B[12] + A[7]*B[13] + A[11]*B[14] + A[15]*B[15]
+  };
 
   return ret;
 }
 
-gmath::Mat4 gmath::mat4::ortho(float left, float right, float bottom, float top, float z_near, float z_far)
+mat4 mat4::ortho(float left, float right, float bottom, float top, float z_near, float z_far)
 {
   float tx, ty, tz;
   tx = -(right+left)/(right-left);
   ty = -(top+bottom)/(top-bottom);
   tz = -(z_far+z_near)/(z_far-z_near);
 
-  gmath::Mat4 ret;
-  ret.m00 = 2.0f/(right-left);
-  ret.m05 = 2.0f/(top-bottom);
-  ret.m11 = -2.0f/(z_far-z_near);
-  ret.m12 = tx;
-  ret.m13 = ty;
-  ret.m14 = tz;
+  mat4 ret;
+  ret.elements[0] = 2.0f/(right-left);
+  ret.elements[5] = 2.0f/(top-bottom);
+  ret.elements[11] = -2.0f/(z_far-z_near);
+  ret.elements[12] = tx;
+  ret.elements[13] = ty;
+  ret.elements[14] = tz;
 
   return ret;
 }
+
+} // namespace gmath
