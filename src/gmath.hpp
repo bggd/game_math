2d2c44a4b66b354119cb8156fe492a8b50b7ebb0
bggd <gameovera@live.com>
2017-12-12 19:02:05 +0900

    include all files in src/game.hpp

--- src/gmath.hpp
+++ src/gmath.hpp
@@ -1,2 +1,13 @@
-namespace gmath {
-} // namespace gmath
+#pragma once
+
+
+#include "vec4.hpp"
+#include "vec3.hpp"
+#include "mat4.hpp"
+
+
+#ifdef GAME_MATH_IMPLEMENTATION
+#include "vec4.cpp"
+#include "vec3.cpp"
+#include "mat4.cpp"
+#endif
