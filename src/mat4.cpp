6b7c840f0194584411ad342e529a07848f9b1f93
bggd <gameovera@live.com>
2018-09-05 12:05:46 +0900

    code styling

--- src/mat4.cpp
+++ src/mat4.cpp
@@ -84,7 +84,7 @@ mat4 mat4::perspective(float y_fov, float aspect, float z_near, float z_far)
   return {
     vec4(f/aspect, 0.0f, 0.0f, 0.0f),
     vec4(0.0f, f, 0.0f, 0.0f),
-    vec4(0.0f, 0.0f, -((z_far+z_near) / (z_far-z_near)), -1.0f),
+    vec4(0.0f, 0.0f, -((z_far+z_near)/(z_far-z_near)), -1.0f),
     vec4(0.0f, 0.0f, -((2*z_far*z_near)/(z_far-z_near)), 0.0f)
   };
 }
