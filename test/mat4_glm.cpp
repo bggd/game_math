a6e3d85da7e69432ad3d07ec07780ce799a3514d
bggd <gameovera@live.com>
2018-09-22 22:22:01 +0900

    Add test

--- test/mat4_glm.cpp
+++ test/mat4_glm.cpp
@@ -37,4 +37,17 @@ BOOST_AUTO_TEST_CASE(test_mat4_glm, *boost::unit_test::tolerance<float>(0.000001
   glm_M = glm::ortho(0.0f, 640.0f, 480.0f, 0.0f, 0.1f, 100.0f);
 
   BOOST_TEST(std::vector<float>(M.elements, M.elements+16) == std::vector<float>(glm::value_ptr(glm_M), glm::value_ptr(glm_M)+16), boost::test_tools::per_element());
+
+  float angle = glm::radians(90.0f);
+  M = gmath::mat4::rotate(angle, gmath::vec3(0.0f, 0.0f, 1.0f));
+  gmath::mat4 V = gmath::mat4::look_at(gmath::vec3(3.0f, 4.0f, 5.0f), gmath::vec3(0.0f, 0.0f, 0.0f), gmath::vec3(0.0f, 0.0f, 1.0f));
+  gmath::mat4 P = gmath::mat4::perspective(angle, 4.0f / 3.0f, 0.1f, 1000.0f);
+  gmath::mat4 MVP = gmath::mat4::mul(P, gmath::mat4::mul(V, M));
+
+  glm_M = glm::rotate(glm::mat4(1.0f), angle, glm::vec3(0.0f, 0.0f, 1.0f));
+  glm::mat4 glm_V = glm::lookAt(glm::vec3(3.0f, 4.0f, 5.0f), glm::vec3(0.0f), glm::vec3(0.0f, 0.0f, 1.0f));
+  glm::mat4 glm_P = glm::perspective(angle, 4.0f / 3.0f, 0.1f, 1000.0f);
+  glm::mat4 glm_MVP = glm_P * glm_V * glm_M;
+
+  BOOST_TEST(std::vector<float>(MVP.elements, MVP.elements+16) == std::vector<float>(glm::value_ptr(glm_MVP), glm::value_ptr(glm_MVP)+16), boost::test_tools::per_element());
 }
